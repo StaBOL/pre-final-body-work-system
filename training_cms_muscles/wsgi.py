89ac92dfadeec9712cab5819313d704ef6a9260e
StaBOL <istasyan07@gmail.com>
2026-05-10 01:24:48 +0300

    Update wsgi.py

--- training_cms_muscles/wsgi.py
+++ training_cms_muscles/wsgi.py
@@ -1,7 +1,3 @@
-# wsgi.py
 from backend.app import create_app
 
-# Создаем экземпляр приложения.
-# Переменная должна называться 'application', так как
-# Gunicorn ищет по умолчанию именно её.
 application = create_app()
