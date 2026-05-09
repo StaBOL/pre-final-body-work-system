5a204fd7744d45fd07338ef9cbd9bfa880000832
StaBOL <istasyan07@gmail.com>
2026-05-10 02:35:46 +0300

    Delete training_cms_muscles directory

--- training_cms_muscles/backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt-secret')
-    # Берём адрес базы из переменной окружения
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
--- training_cms_muscles/wsgi.py
+++
@@ -1,3 +0,0 @@
-from backend.app import create_app
-
-application = create_app()
