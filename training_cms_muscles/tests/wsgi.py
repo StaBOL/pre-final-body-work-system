7474e41fcbd354cf9dda57ae5b8d01729d3c0560
StaBOL <istasyan07@gmail.com>
2026-05-10 00:39:04 +0300

    Delete training_cms_muscles/tests/wsgi.py

--- training_cms_muscles/tests/wsgi.py
+++
@@ -1,7 +0,0 @@
-# wsgi.py
-from backend.app import create_app
-
-# Создаем экземпляр приложения.
-# Переменная должна называться 'application', так как
-# Gunicorn ищет по умолчанию именно её.
-application = create_app()
